439ec9871f45da4c3112dc69e382d473c161b77d
MiHiR1296 <mihirbotle@users.noreply.github.com>
2026-01-23 18:59:16 +0530

    fix: TypeScript error in TeamPINInput ref callback

--- components/TeamPINInput.tsx
+++ components/TeamPINInput.tsx
@@ -87,7 +87,7 @@ export default function TeamPINInput({
         {digits.map((digit, index) => (
           <input
             key={index}
-            ref={(el) => (inputRefs.current[index] = el)}
+            ref={(el) => { inputRefs.current[index] = el; }}
             type="text"
             inputMode="numeric"
             maxLength={1}
